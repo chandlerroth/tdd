87824e0fa5a9c6452956485f03e6972190d54972
Chandler Roth <chandler@enok.co>
2019-09-26 15:19:34 -0400

    Add assemble, act, assert

--- src/SignIn/index.test.js
+++ src/SignIn/index.test.js
@@ -4,12 +4,15 @@ import { render, fireEvent } from '@testing-library/react'
 
 describe('SignIn', () => {
     it('should allow user to put in password', () => {
+        // Assemble
         const { getByLabelText } = render(<SignIn />)
         const passwordField = getByLabelText(/password/i)
         const expectedPassword = 'superpassword'
 
+        // Act
         fireEvent.change(passwordField, { target: { value: expectedPassword }})
 
+        // Assert
         expect(passwordField.value).toBe(expectedPassword)
     })
 })
